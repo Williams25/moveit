298fa989c57c5886dd4e5bf2b2f8dc41d381ad5b
Williams25 <william.souza.timao@hotmail.com>
2021-02-24 21:15:03 -0300

    Passando Context Provider para os components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../src/styles/global.css"
 import Head from "next/head"
+import { ChallengesContext } from "../src/contexts/ChallengeContext"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+
+    <ChallengesContext>
       <Head>
         <title>Moveit</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ChallengesContext>
+
   )
 }
 
