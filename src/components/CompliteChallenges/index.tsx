47ab4b361a40eece915ef17555bf5e3d258684b4
Williams25 <william.souza.timao@hotmail.com>
2021-02-24 21:16:27 -0300

    Finalizando a estrutura do component

--- src/components/CompliteChallenges/index.tsx
+++ src/components/CompliteChallenges/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react"
+import { Context } from "../../contexts/ChallengeContext"
 import styles from "./CompliteChallenges.module.css"
 
 export const CompliteChallenges = () => {
+  const { challengesCompleted } = useContext(Context)
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   )
 }
