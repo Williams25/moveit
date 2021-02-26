fc522225925687931e5c3f22d2818bb490c1bc12
Williams25 <william.souza.timao@hotmail.com>
2021-02-26 20:54:26 -0300

    Retirando o contextprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,14 @@
 import "../src/styles/global.css"
 import Head from "next/head"
-import { ChallengesProvider } from "../src/contexts/ChallengeContext"
-import { CountdownProvider } from "../src/contexts/CountdownContext"
 
 function MyApp({ Component, pageProps }) {
   return (
-
-    <ChallengesProvider>
-      <CountdownProvider>
-        <Head>
-          <title>Moveit</title>
-        </Head>
-        <Component {...pageProps} />
-      </CountdownProvider>
-    </ChallengesProvider>
-
+    <>
+      <Head>
+        <title>Moveit</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
