c6c361b217441b027a837f4620c54df0da5475fb
Williams25 <william.souza.timao@hotmail.com>
2021-02-26 20:54:47 -0300

    Adicionando o contextprovider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,77 @@
 import Head from "next/head"
+import { GetServerSideProps } from "next"
 import { ChallengeBox } from "../src/components/ChallengeBox"
 import { CompliteChallenges } from "../src/components/CompliteChallenges"
 import { Countdown } from "../src/components/Countdown"
 import { ExperienceBar } from "../src/components/ExperienceBar"
 import { Profile } from "../src/components/Profile"
-
+import { ChallengesProvider } from "../src/contexts/ChallengeContext"
+import { CountdownProvider } from "../src/contexts/CountdownContext"
+import { motion } from "framer-motion"
 import styles from "../src/styles/Home.module.css"
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home({ challengesCompleted, currentExperience, level }: HomeProps): JSX.Element {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home | Moveit</title>
-      </Head>
+    <ChallengesProvider
+      challengesCompleted={challengesCompleted}
+      currentExperience={currentExperience}
+      level={level}
+    >
+      <CountdownProvider>
+        <div className={styles.container}>
+          <Head>
+            <title>Home | Moveit</title>
+          </Head>
 
-      <ExperienceBar />
+          <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompliteChallenges />
-          <Countdown />
+          <section>
+            <motion.div
+              transition={{ delay: 0, duration: 0.8 }}
+              variants={{
+                show: { opacity: 1, y: '0' },
+                hidden: { opacity: 0, y: '-100%' },
+              }}
+              initial="hidden"
+              animate="show"
+            >
+              <Profile />
+              <CompliteChallenges />
+              <Countdown />
+            </motion.div>
+
+            <motion.div
+              transition={{ delay: 0, duration: 1 }}
+              variants={{
+                show: { opacity: 1, y: '0' },
+                hidden: { opacity: 0, y: '100%' },
+              }}
+              initial="hidden"
+              animate="show"
+            >
+              <ChallengeBox />
+            </motion.div>
+          </section>
         </div>
-
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </div>
+      </CountdownProvider>
+    </ChallengesProvider>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { level, currentExperience, challengesCompleted } = context.req.cookies
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
