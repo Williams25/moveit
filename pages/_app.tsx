604584814f3d2012fa4315cfd56df0961f6a1d19
Williams25 <william.souza.timao@hotmail.com>
2021-02-28 19:19:03 -0300

    Adicionando component Menubar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import "../src/styles/global.css"
 import Head from "next/head"
+import { MenuBar } from "../src/components/MenuBar"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <MenuBar>
       <Head>
         <title>Moveit</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </MenuBar>
   )
 }
 
