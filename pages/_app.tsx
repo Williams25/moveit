2a7343ef59e041c18c8ee6e1ec567578ad3ead86
Williams25 <william.souza.timao@hotmail.com>
2021-03-02 18:29:32 -0300

    Inserindo context do menubar para todos os compoenents terem interação com o menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../src/styles/global.css"
 import Head from "next/head"
 import { MenuBar } from "../src/components/MenuBar"
+import { MenuBarProvider } from "../src/contexts/MenuBarContext"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MenuBar>
-      <Head>
-        <title>Moveit</title>
-      </Head>
-      <Component {...pageProps} />
-    </MenuBar>
+    <MenuBarProvider>
+      <MenuBar>
+        <Head>
+          <title>Moveit</title>
+        </Head>
+        <Component {...pageProps} />
+      </MenuBar>
+    </MenuBarProvider>
   )
 }
 
